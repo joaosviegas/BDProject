6aee28216cf12d8a643f4a8be488663d5af56e56
oPeras1 <hugsouper785@gmail.com>
2025-06-14 02:13:57 +0100

    Add large SQL file with LFS

--- entrega-bd-02-76/data/aeroporto.sql
+++ entrega-bd-02-76/data/aeroporto.sql
@@ -1,16 +1,3 @@
---Popula a tabela aeroporto
-
-INSERT INTO aeroporto (codigo, nome, cidade, pais) VALUES
-  ('LIS', 'Humberto Delgado Airport', 'Lisboa', 'Portugal'),
-  ('OPO', 'Francisco Sá Carneiro Airport', 'Porto', 'Portugal'),
-  ('FAO', 'Faro Airport', 'Faro', 'Portugal'),
-  ('BJZ', 'Badajoz Airport', 'Badajoz', 'Espanha'),
-  ('MAD', 'Adolfo Suárez Madrid–Barajas', 'Madrid', 'Espanha'),
-  ('BCN', 'Barcelona-El Prat Airport', 'Barcelona', 'Espanha'),
-  ('CDG', 'Charles de Gaulle Airport', 'Paris', 'França'),
-  ('ZRH', 'Zurich Airport', 'Zurique', 'Suíça'),
-  ('AMS', 'Amsterdam Schiphol Airport', 'Amsterdam', 'Países Baixos'),
-  ('LHR', 'Heathrow Airport', 'Londres', 'Reino Unido'),
-  ('LGW', 'Gatwick Airport', 'Londres', 'Reino Unido'),
-  ('MXP', 'Milano Malpensa Airport', 'Milão', 'Itália'),
-  ('LIN', 'Milano Linate Airport', 'Milão', 'Itália');
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a7616b91fdd4cf9d3d185179039514b125883fe2b667918d210140168a89740
+size 856
--- entrega-bd-02-76/data/aviao.sql
+++ entrega-bd-02-76/data/aviao.sql
@@ -1,16 +1,3 @@
--- Popula a tabela aviao
-
--- Airbus A320: A320-001 to A320-005
-INSERT INTO aviao (no_serie, modelo)
-SELECT 'A320-' || LPAD(i::text, 3, '0'), 'Airbus A320'
-FROM generate_series(1, 5) AS s(i);
-
--- Boeing 737: B737-001 to B737-005
-INSERT INTO aviao (no_serie, modelo)
-SELECT 'B737-' || LPAD(i::text, 3, '0'), 'Boeing 737'
-FROM generate_series(1, 5) AS s(i);
-
--- Boeing 787: B787-001 to B787-002
-INSERT INTO aviao (no_serie, modelo)
-SELECT 'B787-' || LPAD(i::text, 3, '0'), 'Boeing 787'
-FROM generate_series(1, 2) AS s(i);
+version https://git-lfs.github.com/spec/v1
+oid sha256:53f1a2bfab074d4aeb08f570726e3f330f0fb62da1cd36bfad9a3092148eb0bd
+size 519
